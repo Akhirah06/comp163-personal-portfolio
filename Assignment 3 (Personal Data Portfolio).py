600d795457d28707dcede72f2c3ea13d19c298ab
ahhkiivci <dexxnattionn@gmail.com>
2025-09-19 22:00:18 -0400

    Add real academic data and course information

--- Assignment 3 (Personal Data Portfolio).py
+++ Assignment 3 (Personal Data Portfolio).py
@@ -4,3 +4,9 @@ student_email = "dihenry@ncat.edu"
 student_hometown = "Atlanta, GA"
 student_grad_semester = "Spring 2029"
 student_major = "Computer Science"
+
+#Academic Data Organizational
+current_courses = ["COMP 163", "MATH 131", "SPCH 250", "HIST 106", "GEEN 111", "COMP 121"]
+completed_courses = ["Psychology", "Physics", "College Algebra", "Elementary Japanese", "World History"]
+credit_hours = [3, 4, 1, 1, 3, 3]
+gpa_history = [3.2, 3.6, 3.4, 3.7]
